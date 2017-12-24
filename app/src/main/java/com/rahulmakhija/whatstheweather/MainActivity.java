5b1c13270e31f3cb01e298b6547b82499eea4b25
Rahul Makhija <rahuls360@gmail.com>
2017-12-24 16:20:47 +0530

    Display weather based on user input

--- app/src/main/java/com/rahulmakhija/whatstheweather/MainActivity.java
+++ app/src/main/java/com/rahulmakhija/whatstheweather/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -18,34 +20,18 @@ import java.net.URL;
 
     public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textView);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
 
-        DownloadWebsite downloadWebsite = new DownloadWebsite();
-        try {
-            String result = downloadWebsite.execute("http://api.openweathermap.org/data/2.5/weather?q=Bangalore&appid=b629d61c3883de621e8fb5a8ff111b30&units=metric").get();
-            Log.i("Result", ""+result);
 
-            JSONObject jsonObject = new JSONObject(result);
-            JSONArray weather = jsonObject.getJSONArray("weather");
-            JSONObject firstObject = weather.getJSONObject(0);
-            String description = firstObject.getString("description");
-            Log.i("Description", description);
-
-            JSONObject main = jsonObject.getJSONObject("main");
-            int temperature = main.getInt("temp");
-            Log.i("Temperature", "" + temperature);
-
-            textView.setText("Bangalore's Temperature = " + temperature + " and the weather is " + description);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.i("Result", "Failure");
-        }
     }
 
     public class DownloadWebsite extends AsyncTask<String,Void,String>{
@@ -80,6 +66,32 @@ import java.net.URL;
 
 
             return null;
+        }
+    }
+
+    public void checkWeather(View view){
+        String cityName = editText.getText().toString();
+
+        DownloadWebsite downloadWebsite = new DownloadWebsite();
+        try {
+            String result = downloadWebsite.execute("http://api.openweathermap.org/data/2.5/weather?appid=b629d61c3883de621e8fb5a8ff111b30&units=metric&q=" + cityName).get();
+            Log.i("Result", ""+result);
+
+            JSONObject jsonObject = new JSONObject(result);
+            JSONArray weather = jsonObject.getJSONArray("weather");
+            JSONObject firstObject = weather.getJSONObject(0);
+            String description = firstObject.getString("description");
+            Log.i("Description", description);
+
+            JSONObject main = jsonObject.getJSONObject("main");
+            int temperature = main.getInt("temp");
+            Log.i("Temperature", "" + temperature);
+
+            textView.setText( cityName + "'s Temperature = " + temperature + "\nWeather Description: " + description);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("Result", "Failure");
         }
     }
 }
